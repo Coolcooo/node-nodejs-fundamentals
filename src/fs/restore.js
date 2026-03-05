e19ef7b619e8c1f57a44dbaa5e1683d10efc7d57
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-03-06 00:33:06 +0300

    feat: implement restore function

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,34 @@
+import fs from "fs/promises";
+import path from "path";
+import {throwIsExist, throwIsNotExist} from "./utils/throw-error.js";
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  const restoreWorkspacePath = path.resolve("../../workspace_restored");
+  const snapshotPath = path.resolve("../../snapshot.json");
+
+	await Promise.all([throwIsExist(restoreWorkspacePath), throwIsNotExist(snapshotPath)]);
+
+	await fs.mkdir(restoreWorkspacePath);
+
+	const shapshotContent = await fs.readFile(snapshotPath, {encoding: "utf8"});
+	const snapshot = JSON.parse(shapshotContent);
+
+	for (const fileInfo of snapshot.entries) {
+		const filePath = path.join(restoreWorkspacePath, fileInfo.path);
+		switch (fileInfo.type) {
+			case "file": {
+				await fs.writeFile(filePath, fileInfo.content, {encoding: "base64"});
+				break;
+			}
+			case "directory": {
+				await fs.mkdir(filePath);
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+	}
 };
 
 await restore();
