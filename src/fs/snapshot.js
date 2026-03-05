ffdecc46e917bb23da2d281fac75c20419687d18
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-03-06 00:01:55 +0300

    feat: implement snapshot function

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -2,11 +2,37 @@ import fs from "fs/promises";
 import path from "node:path";
 
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+	const rootPath = path.resolve("../../workspace");
+
+	try {
+		const rootStats = await fs.stat(rootPath);
+	} catch (e) {
+		throw new Error("FS operation failed");
+	}
+
+	const result = {
+		rootPath: rootPath,
+		entries: []
+	};
+
+	const files = await fs.readdir(rootPath, {recursive: true});
+  for (const file of files) {
+		const absolutePath = path.join(rootPath, file);
+		const fileInfo = {path: file};
+		const stats = await fs.stat(absolutePath);
+
+		if (stats.isFile()) {
+			fileInfo.type = "file";
+			fileInfo.size = stats.size;
+			fileInfo.content = await fs.readFile(absolutePath, {encoding: "base64"});
+		} else {
+			fileInfo.type = "directory";
+		}
+
+		result.entries.push(fileInfo);
+	}
+
+	fs.writeFile("../../snapshot.json", JSON.stringify(result));
 };
 
 await snapshot();
