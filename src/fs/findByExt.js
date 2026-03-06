776ff1d492aa18ddf6edcc3552a3a40618eaca72
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-03-06 20:17:34 +0300

    feat: add implement findByExt

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -1,7 +1,37 @@
+import process from "process";
+import path from "path";
+import fs from "fs/promises";
+import {getDirname} from "./utils/dirname.js";
+
+
+const getArgValue = (args, expectedArg) => {
+	for (let i = 0; i < args.length; i += 1) {
+		const arg = args[i];
+		if (arg === expectedArg) {
+			const value = args[i + 1];
+			if (typeof value === "string") {
+				return value;
+			}
+		}
+	}
+	return null;
+}
+
 const findByExt = async () => {
-  // Write your code here
-  // Recursively find all files with specific extension
-  // Parse --ext CLI argument (default: .txt)
+	const processExt = getArgValue(process.argv, "--ext");
+	const resultExt = "." + (processExt !== null ? processExt : "txt");
+  const workspacePath = path.join(getDirname(import.meta.url), "../../workspace");
+	const files = await fs.readdir(workspacePath, {recursive: true});
+	const filesByExt = [];
+	for (const file of files) {
+		if (path.extname(file) === resultExt) {
+			filesByExt.push(file);
+		}
+	}
+	filesByExt.sort();
+	for (const file of filesByExt) {
+		console.log(file);
+	}
 };
 
 await findByExt();
