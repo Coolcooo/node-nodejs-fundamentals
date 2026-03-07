a7e7f2fa763293fc3ac5b3c2eddc86e2239925f3
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-03-08 00:06:40 +0300

    fix: remove readline interface and use CLI arg

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,32 +1,26 @@
-import readline from "readline";
-
 const dynamic = async () => {
-	const rl = readline.createInterface({
-		input: process.stdin
-	});
-	for await (const line of rl) {
-		let module;
-		switch (line) {
-			case "repeat": {
-				module = await import("./plugins/repeat.js");
-				break;
-			}
-			case "reverse": {
-				module = await import("./plugins/reverse.js");
-				break;
-			}
-			case "uppercase": {
-				module = await import("./plugins/uppercase.js");
-				break;
-			}
-			default: {
-				console.log("Plugin not found");
-				process.exit(1);
-				return;
-			}
+	const moduleName = process.argv[2];
+	let module;
+	switch (moduleName) {
+		case "repeat": {
+			module = await import("./plugins/repeat.js");
+			break;
 		}
-		console.log(module.run());
+		case "reverse": {
+			module = await import("./plugins/reverse.js");
+			break;
+		}
+		case "uppercase": {
+			module = await import("./plugins/uppercase.js");
+			break;
+		}
+		default: {
+			console.log("Plugin not found");
+			process.exit(1);
+			return;
+		}
 	}
+	console.log(module.run());
 };
 
 await dynamic();
