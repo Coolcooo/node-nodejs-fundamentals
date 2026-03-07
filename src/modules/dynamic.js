20b2ed88dc680e733a3561865431bbf719a991ee
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-03-08 00:01:55 +0300

    feat: implement dynamic.js

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,32 @@
+import readline from "readline";
+
 const dynamic = async () => {
-  // Write your code here
-  // Accept plugin name as CLI argument
-  // Dynamically import plugin from plugins/ directory
-  // Call run() function and print result
-  // Handle missing plugin case
+	const rl = readline.createInterface({
+		input: process.stdin
+	});
+	for await (const line of rl) {
+		let module;
+		switch (line) {
+			case "repeat": {
+				module = await import("./plugins/repeat.js");
+				break;
+			}
+			case "reverse": {
+				module = await import("./plugins/reverse.js");
+				break;
+			}
+			case "uppercase": {
+				module = await import("./plugins/uppercase.js");
+				break;
+			}
+			default: {
+				console.log("Plugin not found");
+				process.exit(1);
+				return;
+			}
+		}
+		console.log(module.run());
+	}
 };
 
 await dynamic();
