504affee862297c4339408959217410c6e02728a
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-03-08 19:11:54 +0300

    feat: implement interactive.js

--- src/cli/interactive.js
+++ src/cli/interactive.js
@@ -1,8 +1,43 @@
+import readline from "readline";
+import path from "path";
+
+const startTime = performance.now();
 const interactive = () => {
-  // Write your code here
-  // Use readline module for interactive CLI
-  // Support commands: uptime, cwd, date, exit
-  // Handle Ctrl+C and unknown commands
+  const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	rl.on("close", () => {
+		console.log("Goodbye!");
+	});
+
+	rl.on("line", (line) => {
+		switch (line) {
+			case "uptime": {
+				const seconds = Math.round((performance.now() - startTime) / 10) /100;
+				console.log(`Uptime: ${seconds}s`);
+				break;
+			}
+			case "cwd": {
+				console.log(path.resolve("./"));
+				break;
+			}
+			case "date": {
+				console.log((new Date()).toISOString());
+				break;
+			}
+			case "exit": {
+				rl.close();
+				return;
+			}
+			default: {
+				console.log("Unknown command");
+				break;
+			}
+		}
+		rl.prompt();
+	});
 };
 
 interactive();
