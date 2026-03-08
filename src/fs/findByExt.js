7f62276cbfff8876944830257e9c8340ce1840d9
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-03-09 02:46:09 +0300

    fix: throw error if workspace path is not exists

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -2,10 +2,12 @@ import process from "process";
 import path from "path";
 import fs from "fs/promises";
 import {getStringArgValue} from "../utils/process.js";
+import {throwIsNotExist} from "../utils/throw-error.js";
 
 const findByExt = async () => {
 	const resultExt = "." + getStringArgValue(process.argv, "--ext", "txt");
   const workspacePath = path.resolve(import.meta.dirname, "../../workspace");
+	await throwIsNotExist(workspacePath);
 	const files = await fs.readdir(workspacePath, {recursive: true});
 	const filesByExt = [];
 	for (const file of files) {
